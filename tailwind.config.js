67b8132ba55bc82e71349f0b536f660f53f5cb1e
Jeremy Ong <jeremycong@gmail.com>
2022-04-04 19:17:12 -0600

    Cleanup and fix tailwind build

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,10 @@ module.exports = {
   content: [
     'index.html',
     './dist/*.txt',
-    './src/**/*.{html,js}',
-    './node_modules/tw-elements/dist/js/**/*.js',
-    './node_modules/flowbite/**/*.js'
+    './src/**/*.{html,js}'
   ],
   theme: {
     extend: {},
   },
-  // plugins: [require('daisyui')],
-  plugins: [
-    // require('tw-elements/dist/plugin')
-    // require('flowbite/plugin')
-  ]
+  plugins: []
 }
